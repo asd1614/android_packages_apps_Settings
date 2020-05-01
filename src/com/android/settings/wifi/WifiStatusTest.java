67bda0d9aa11e25777e0a8722cb0a5215bd271ff
jageryzhong <jageryzhong@outlook.com>
2020-05-01 23:02:54 +0800

    switch ping host google.com to baidu.com for China
    Change-Id: I95f988aacc01918030d8a4d031d2499c7d4a7a3f

--- src/com/android/settings/wifi/WifiStatusTest.java
+++ src/com/android/settings/wifi/WifiStatusTest.java
@@ -343,7 +343,7 @@ public class WifiStatusTest extends Activity {
     private final void pingHostname() {
         try {
             // TODO: Hardcoded for now, make it UI configurable
-            Process p = Runtime.getRuntime().exec("ping -c 1 -w 100 www.google.com");
+            Process p = Runtime.getRuntime().exec("ping -c 1 -w 100 www.baidu.com");
             int status = p.waitFor();
             if (status == 0) {
                 mPingHostnameResult = "Pass";
@@ -363,7 +363,7 @@ public class WifiStatusTest extends Activity {
         HttpURLConnection urlConnection = null;
         try {
             // TODO: Hardcoded for now, make it UI configurable
-            URL url = new URL("https://www.google.com");
+            URL url = new URL("https://www.baidu.com");
             urlConnection = (HttpURLConnection) url.openConnection();
             if (urlConnection.getResponseCode() == 200) {
                 mHttpClientTestResult = "Pass";
